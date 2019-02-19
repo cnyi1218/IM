c59df5dd4bff45abcea9c17ca431a33eb64a130d
cnyi1218 <1014436509@qq.com>
2019-02-19 21:05:26 +0800

    splash View层实现

---
+++ app/src/main/java/haoruyi/com/LoginActivity.kt
@@ -0,0 +1,5 @@
+package haoruyi.com
+
+class LoginActivity :BaseActivity() {
+    override fun getLayoutResId(): Int =R.layout.activity_login
+}
--- app/src/main/java/haoruyi/com/SplashActivity.kt
+++ app/src/main/java/haoruyi/com/SplashActivity.kt
@@ -1,7 +1,35 @@
 package haoruyi.com
 
-class SplashActivity :BaseActivity() {
+import android.os.Handler
+import haoruyi.com.contract.SplashContract
+import org.jetbrains.anko.startActivity
+
+class SplashActivity :BaseActivity(),SplashContract.View {
+
+    companion object {
+        val DELAY = 2000L
+    }
+
+    val handler by lazy {
+        Handler()
+    }
+
     override fun getLayoutResId(): Int =
         R.layout.activity_splash
+
+
+    //延时2秒，跳到登录界面
+    override fun onNotLoggedIn() {
+        handler.postDelayed({
+            startActivity<LoginActivity>()
+            finish()
+        }, DELAY)
+    }
+
+    //跳转到主界面
+    override fun onLoggedIn() {
+        startActivity<MainActivity>()
+        finish()
+    }
 
 }
