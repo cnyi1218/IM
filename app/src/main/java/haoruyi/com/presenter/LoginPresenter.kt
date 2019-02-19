65ebb7faa57ef9dc1dbad848d7fa14169e5c623c
cnyi1218 <1014436509@qq.com>
2019-02-20 01:14:15 +0800

    登录界面Model层实现

--- app/src/main/java/haoruyi/com/presenter/LoginPresenter.kt
+++ app/src/main/java/haoruyi/com/presenter/LoginPresenter.kt
@@ -1,5 +1,8 @@
 package haoruyi.com.presenter
 
+import com.hyphenate.EMCallBack
+import com.hyphenate.chat.EMClient
+import haoruyi.com.adapter.EMCallBackAdapter
 import haoruyi.com.contract.LoginContract
 import haoruyi.com.extentions.isValidPassword
 import haoruyi.com.extentions.isValidUserName
@@ -19,6 +22,20 @@ class LoginPresenter(val view:LoginContract.View):LoginContract.Presenter{
     }
 
     private fun loginEaseMob(userName: String, password: String) {
+        EMClient.getInstance().login(userName,password,object : EMCallBackAdapter() {
+
+            //在子线程回调
+            override fun onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                //在主线程通知View层
+                uiThread { view.onLoggedInSuccess() }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                uiThread { view.onLoggedInFailed() }
+            }
+        })
 
     }
 
