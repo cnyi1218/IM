a3e6221e1627d89e8398ef80f2d90230bf7be9e1
cnyi1218 <1014436509@qq.com>
2019-02-25 00:15:16 +0800

    注册界面view层的实现

--- app/src/main/java/haoruyi/com/RegisterActivity.kt
+++ app/src/main/java/haoruyi/com/RegisterActivity.kt
@@ -1,6 +1,36 @@
 package haoruyi.com
 
-class RegisterActivity :BaseActivity(){
+import haoruyi.com.contract.RegisterContract
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
+
+class RegisterActivity :BaseActivity(),RegisterContract.View{
+    override fun onUserNameError() {
+        userName.error = getString(R.string.user_name_error)
+    }
+
+    override fun onPasswordError() {
+        password.error = getString(R.string.password_error)
+    }
+
+    override fun onConfirmPasswordError() {
+        confirmPassword.error = getString(R.string.confirm_password_error)
+    }
+
+    override fun onStartRegister() {
+        showProgress(getString(R.string.registering))
+    }
+
+    override fun onRegisterSuccess() {
+        dissmissProgress()
+        finish()
+    }
+
+    override fun onRegisterFailed() {
+        dissmissProgress()
+        toast(R.string.register_failed)
+    }
+
     override fun getLayoutResId(): Int =R.layout.activity_register
 
 }
